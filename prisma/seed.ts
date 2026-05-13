a86b28e12463d01bfc9c0766d2583f2ff5e368a6
AKA2d <aka2ds00@gmail.com>
2026-05-13 15:15:32 +0330

    updated seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,5 @@
+//seed one admin and user to db
+
 import "dotenv/config";
 import { z } from "zod";
 import { Role } from "../src/generated/prisma/enums";
@@ -7,26 +9,43 @@ import { prisma } from "../src/lib/prisma";
 const seedEnv = z.object({
   ADMIN_USERNAME: z.string().min(1).default("admin"),
   ADMIN_PASSWORD: z.string().min(12).default("ChangeMeNow123!"),
+  USER_USERNAME:  z.string().min(1).default("user"),
+  USER_PASSWORD:  z.string().min(12).default("ChangeMeNow123!"),
 });
 
 async function main() {
   const env = seedEnv.parse(process.env);
-  const password = env.ADMIN_PASSWORD;
+  const admin_password = env.ADMIN_PASSWORD;
+  const user_password = env.USER_PASSWORD;
 
   await prisma.user.upsert({
     where: { username: env.ADMIN_USERNAME },
     update: {
-      passwordHash: await hashPassword(password),
+      passwordHash: await hashPassword(admin_password),
       role: Role.ADMIN,
     },
     create: {
       username: env.ADMIN_USERNAME,
-      passwordHash: await hashPassword(password),
+      passwordHash: await hashPassword(admin_password),
       role: Role.ADMIN,
     },
   });
 
   console.log(`Seeded admin user: ${env.ADMIN_USERNAME}`);
+
+  await prisma.user.upsert({
+    where: { username: env.USER_USERNAME },
+    update: {
+      passwordHash: await hashPassword(user_password),
+      role: Role.VIEWER,
+    },
+    create: {
+      username: env.USER_USERNAME,
+      passwordHash: await hashPassword(user_password),
+      role: Role.VIEWER,
+    },
+  });
+  console.log(`Seeded normal user: ${env.USER_USERNAME}`);
 }
 
 main()
